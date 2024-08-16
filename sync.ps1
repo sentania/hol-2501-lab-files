3a9cbeab0044b0980030acd105b1e00c7c37aee5
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2024-08-16 21:46:26 +0100

    Added cleanup script

--- sync.ps1
+++
@@ -1,4 +0,0 @@
-Set-Location "c:\"
-Get-ChildItem -path ".\labfiles" -Directory -Recurse -Filter ".git" -Hidden | Remove-Item -Force -Recurse
-Get-ChildItem -path ".\labfiles" -Recurse -Filter ".gitignore" -Hidden | Remove-Item -Force -Recurse
-Get-ChildItem -path ".\labfiles" -Recurse -Filter "README.md" -Hidden | Remove-Item -Force -Recurse
