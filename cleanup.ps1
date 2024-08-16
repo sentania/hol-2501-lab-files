20550d0114b2e9a5721f878f91139482bea3647e
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2024-08-17 00:36:21 +0100

    removed hidden flag where not relevant

--- cleanup.ps1
+++ cleanup.ps1
@@ -1,5 +1,5 @@
 Write-Output "$(Get-Date) Lab Files Clean Up Script"
 
 Get-ChildItem -path "C:\labfiles" -Recurse -Filter ".gitattributes" -Hidden | Remove-Item -Force -Recurse
-Get-ChildItem -path "C:\labfiles" -Recurse -Filter ".gitignore" -Hidden | Remove-Item -Force -Recurse
-Get-ChildItem -path "C:\labfiles" -Recurse -Filter "README.md" -Hidden | Remove-Item -Force -Recurse
+Get-ChildItem -path "C:\labfiles" -Recurse -Filter ".gitignore" | Remove-Item -Force -Recurse
+Get-ChildItem -path "C:\labfiles" -Recurse -Filter "README.md" | Remove-Item -Force -Recurse
