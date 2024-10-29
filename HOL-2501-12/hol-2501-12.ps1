dad9184bfd3d87472ae3a4341f846aa96a066b8a
Christopher Lewis <christopher.lewis@thecloudxpert.net>
2024-10-29 01:54:31 +0000

    Adding more TASKS info and added Bookmark creation

--- HOL-2501-12/hol-2501-12.ps1
+++ HOL-2501-12/hol-2501-12.ps1
@@ -1,23 +1,28 @@
-Write-Output "$(Get-Date) HOL-2501-12 Management Pack Builder PreReqs"
+Write-Host "HOL-2501-12 Management Pack Builder PreReqs - Started" -ForegroundColor Green
+Write-Host "=====================================================" -ForegroundColor Green
+
+# Create Gitlab entries
 
 $list = @('hol-moad','hol-demo')
 $path = "c:\gitlab"
 $origin = "$path\rainpole"
 $user = "buildadmin"
 
-Get-ChildItem -path "c:\gitlab" -Directory -Recurse -Filter ".git" -Hidden | Remove-Item -Force -Recurse
+Write-Host "TASK: Remove Git Repo Flags" -ForegroundColor Yellow
+Get-ChildItem -path $path -Directory -Recurse -Filter ".git" -Hidden | Remove-Item -Force -Recurse | Out-Null
 
+Write-Host "TASK: Create Git Repo(s) and Sync Content" -ForegroundColor Yellow
 $list | ForEach-Object {
     $target = "$path\$_"
     If ( -Not (Test-Path -Path $target))
     {
-        Write-Output "Creating $target folder"
-        New-Item -Path $path -Name $_ -ItemType Directory
+        Write-Host "Creating $target folder"
+        New-Item -Path $path -Name $_ -ItemType Directory | Out-Null
     }
     If (Test-Path -Path $target)
     {
-        Write-Output "Copying files / folders in $origin to $target folder"
-        Copy-Item -Path "$origin\*" -Destination $target -Recurse -Force
+        Write-Host "Copying files / folders in $origin to $target folder"
+        Copy-Item -Path "$origin\*" -Destination $target -Recurse -Force | Out-Null
     
         Set-Location $target
     
@@ -25,6 +30,7 @@ $list | ForEach-Object {
             Invoke-Command -ScriptBlock { git init -b main }
             Invoke-Command -ScriptBlock { git remote add origin https://$user:VMware123!@gitlab.vcf.sddc.lab/$user/$($_) }
         }
+        Write-Host "Pushing $target folder to git"
         Invoke-Command -ScriptBlock { git add . }
         Invoke-Command -ScriptBlock { git commit -m "$(Get-Date) Refresh" }
         Invoke-Command -ScriptBlock { git push -f -u origin main }
@@ -35,6 +41,10 @@ $list | ForEach-Object {
     }
 }
 
+# Create Ref VM
+
+Write-Host "TASK: Create Reference Virtual Machine" -ForegroundColor Yellow
+
 $password = "VMware123!"
 $username = "administrator@vsphere.local"
 $vcName = "vcenter-mgmt.vcf.sddc.lab"
@@ -43,18 +53,42 @@ $machine = "gitlab"
 $EncryptedPassword = ConvertTo-SecureString -String $password -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $EncryptedPassword
 
-Connect-VIserver -Server $vcName -Credential $Credential
+Write-Host "Connecting to $vcName"
+Connect-VIserver -Server $vcName -Credential $Credential | Out-Null
 
-$exists = get-vm -name $machine -ErrorAction SilentlyContinue
+$exists = get-vm -name $machine -ErrorAction SilentlyContinue | Out-Null
 
 if (-Not ($exists)) {
-    $resourcePool = Get-Cluster -Name 'mgmt-cluster-01' | Get-ResourcePool "Resources" 
+    $resourcePool = Get-Cluster -Name 'mgmt-cluster-01' | Get-ResourcePool "Resources"
     $pg = Get-VDPortgroup -Name 'xregion-seg01' -VDSwitch 'mgmt-vds01'
-    New-VM -Name $machine -ResourcePool $resourcePool -NumCpu 1 -MemoryGB 1 -DiskGB 1 -Portgroup $pg
-    if (-Not (Get-VM | where {$_.PowerState -eq $PoweredOn}))
+    Write-Host "Creating $machine VM"
+    New-VM -Name $machine -ResourcePool $resourcePool -NumCpu 1 -MemoryGB 1 -DiskGB 1 -Portgroup $pg | Out-Null
+    if (-Not (Get-VM | Where-Object {$_.PowerState -eq $PoweredOn}))
     {
-        Start-VM $machine
+        Write-Host "Starting $machine"
+        Start-VM $machine | Out-Null
     }
 }
 
+Write-Host "Disconnecting $vcName"
 Disconnect-VIserver -Server * -Confirm:$false
+
+# Create Bookmark
+
+Write-Host "TASK: Create Management Pack Builder Bookmark" -ForegroundColor Yellow
+
+$registryPath = "HKLM:\\SOFTWARE\\Policies\\Mozilla\\FireFox\\Bookmarks\22"
+
+Set-ItemProperty $registryPath -Name "Title" -Value "Aria Operations MP Builder" -Type "String"
+Set-ItemProperty $registryPath -Name "URL" -Value "https://mpb.vcf.sddc.lab/login" -Type "String"
+Set-ItemProperty $registryPath -Name "Favicon" -Value "https://mpb.vcf.sddc.lab/favicon.ico" -Type "String"
+Set-ItemProperty $registryPath -Name "Placement" -Value "toolbar" -Type "String"
+Set-ItemProperty $registryPath -Name "Folder" -Value "VCF Cloud Management" -Type "String"
+
+Write-Host "TASK: Run Group Policy Update - Force" -ForegroundColor Yellow
+Invoke-GPUpdate -Force | Out-Null
+
+Write-Host "=======================================================" -ForegroundColor Green
+Write-Host "HOL-2501-12 Management Pack Builder PreReqs - Completed" -ForegroundColor Green
+
+Write-Host "Please restart Firefox for Bookmark to show"
